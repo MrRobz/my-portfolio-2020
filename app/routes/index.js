2a19322a2aa4f3a35fe47764e458385b6091784f
Robin Philip Thomas <robin.thomas@freshworks.com>
2020-05-02 12:50:57 +0530

    contact form

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,14 @@
 import Route from '@ember/routing/route';
+import { getOwner } from '@ember/application';
 
 export default class IndexRoute extends Route {
+  activate() {
+    let lineGenerator = getOwner(this).lookup('controller:application').lineGenerator;
+    lineGenerator.start();
+  }
+  resetController(controller, isExiting) {
+    if (isExiting) {
+      getOwner(this).lookup('controller:application').lineGenerator.stop();
+    }
+  }
 }
